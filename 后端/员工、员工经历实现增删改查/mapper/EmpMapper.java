09af2f779d908d5de536b05df487756454121a4c
litao-hub <2326827935@qq.com>
2025-07-03 15:50:12 +0800

    Mapper层代码实现

---
+++ 后端/员工、员工经历实现增删改查/mapper/EmpMapper.java
@@ -0,0 +1,76 @@
+package com.litao.mapper;
+
+
+import com.litao.pojo.Emp;
+import com.litao.pojo.EmpQueryParam;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 员工信息
+ */
+@Mapper
+public interface EmpMapper {
+
+    //------------------原始分页方法
+    /**
+     * 查询总记录数
+     */
+//    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id")
+//    public Long count();
+
+    /**
+     * 分页查询
+     */
+    //@Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id order by e.update_time desc")
+//    public List<Emp> list(String name, Integer gender, LocalDate begin, LocalDate end);
+
+    /**
+     * 条件查询员工信息
+     * @param empQueryParam
+     * @return
+     */
+      public List<Emp> list(EmpQueryParam empQueryParam);
+
+    /**
+     * 新增员工基本信息
+     */
+    @Options(useGeneratedKeys = true, keyProperty = "id")//主键返回
+    @Insert("insert into emp(username, name, gender, phone, job, salary, image, entry_date, dept_id, create_time, update_time)" +
+            "values (#{username},#{name},#{gender},#{phone},#{job},#{salary},#{image},#{entryDate},#{deptId},#{createTime},#{updateTime})")
+    void insert(Emp emp);
+
+    /**
+     * 根据id批量删除员工的基本信息
+     */
+    void deleteByIds(List<Integer> ids);
+
+    /**
+     * 根据ID查询员工详细信息
+     */
+    Emp getById(Integer id);
+
+    /**
+     * 更新员工基本信息
+     */
+    void updateById(Emp emp);
+
+    /**
+     * 统计各个职位的员工人数
+     */
+    @MapKey("pos")
+    List<Map<String,Object>> countEmpJobData();
+    /**
+     * 统计员工性别信息
+     */
+    @MapKey("name")
+    List<Map> countEmpGenderData();
+    /**
+     * 根据用户名和密码查询员工信息
+     */
+    @Select("select * from emp where username = #{username} and password = #{password}")
+    Emp getUsernameAndPassword(Emp emp);
+}
