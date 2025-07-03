3678e00c14095d3d4e9d4a95ec652a7677b9ecd6
litao-hub <2326827935@qq.com>
2025-07-03 15:21:10 +0800

    Delete java directory

--- java/com/litao/service/impl/EmpServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.litao.service.impl;
-
-import com.litao.service.EmpService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmpServiceImpl implements EmpService {
-}
