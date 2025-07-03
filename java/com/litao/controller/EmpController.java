98cdc5cfcf0265cba0b8289d982455efd48b227f
litao-hub <2326827935@qq.com>
2025-07-03 15:09:27 +0800

    后端部门各层实现总代码

---
+++ java/com/litao/controller/EmpController.java
@@ -0,0 +1,13 @@
+package com.litao.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 员工管理Controller
+ */
+@Slf4j
+@RestController
+public class EmpController {
+}
---
+++ java/com/litao/service/EmpService.java
@@ -0,0 +1,4 @@
+package com.litao.service;
+
+public interface EmpService {
+}
